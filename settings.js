0df4e9f7312b7edaa2394690ae157c645585ab01
SACHIBOT <91013948+SACHIBOT@users.noreply.github.com>
2022-07-08 11:54:58 +0530

    Black-King

---
+++ settings.js
@@ -0,0 +1,74 @@
+//═════════[®SACHIBOT Inc.]═══════════//
+//                                            //
+//             𝙱𝚕𝚊𝚌𝚔𝙺𝚒𝚗𝚐 𝚋𝚢 𝚂𝚊𝚌𝚑𝚒𝙱𝚘𝚝      //
+//                                            //
+//                    𝚆𝚑𝚊𝚝𝚜𝚊𝚙𝚙-𝙼𝙳 𝙱𝚘𝚝       //
+//                                            //
+//═════════════[© 2022]═════════════//
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+//Api Website\\
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+//Api Key\\
+global.APIKeys = {
+	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
+}
+
+//customize settings\\
+global.owner = ['94725881990']
+global.premium = ['94725881990']
+global.ownernomer = '94725881990'
+global.ownername = 'Sachi Bot'
+global.botname = 'Black-King'
+global.footer = '©SachiBotInc.'
+global.ig = 'https://github.com/SACHIBOT'
+global.region = 'Sri Lanka, Western Province, Colombo'
+global.sc = 'https://github.com/SACHIBOT/Black-King'
+global.myweb = 'https://github.com/SACHIBOT/'
+global.packname = 'Black-King'
+global.author = 'Sachi bot'
+global.sessionName = 'session'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: 'Done ✓',
+    admin: 'This Feature Is Only For Admin!',
+    botAdmin: 'Bot Must Be Admin First!',
+    owner: 'This Feature Is Only For Owner!',
+    group: 'Feature Used Only For Groups!',
+    private: 'Features Used Only For Private Chat!',
+    bot: 'This Feature Is Only For Bot',
+    wait: 'Wait A minute...',
+    error: 'Error! Maybe Api Key Is Expired🤔!',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+}
+    global.limitawal = {
+    premium: "Infinity",
+    free: 12,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+global.thumb = fs.readFileSync('./BlackKingMedia/BlackKing.jpg')
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
